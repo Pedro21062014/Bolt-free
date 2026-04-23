f21e74899c7bbb206da0b571aaaa65d943c3a365
Pedro21062014 <166228658+Pedro21062014@users.noreply.github.com>
2026-04-23 21:06:58 +0000

    Atualiza modelos e inicializa lista de modelos no carregador da API

--- app/routes/api.models.ts
+++ app/routes/api.models.ts
@@ -1,6 +1,7 @@
 import { json } from '@remix-run/cloudflare';
-import { MODEL_LIST } from '~/utils/constants';
+import { MODEL_LIST, initializeModelList } from '~/utils/constants';
 
 export async function loader() {
+  await initializeModelList();
   return json(MODEL_LIST);
 }
